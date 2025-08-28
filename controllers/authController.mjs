2a4e436b2436aa67b787fdf5cf91e0162d23a5f2
Hamza Baloch <158695894+Hamzabaloch08@users.noreply.github.com>
2025-08-28 23:41:02 +0500

    Update authController.mjs

--- controllers/authController.mjs
+++ controllers/authController.mjs
@@ -111,7 +111,7 @@ export const logout = async (req, res) => {
     res.clearCookie("token", {
       httpOnly: true,
       secure: isProduction, // local pe false, deploy pe true
-      sameSite: isProduction ? "none" : "lax", // local pe lax, deploy pe none
+      sameSite: isProduction ? "lax" : "node", // local pe lax, deploy pe none
       path: "/", // important: cookie path specify karna
     });
 
