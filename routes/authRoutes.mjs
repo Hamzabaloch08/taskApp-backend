e3a78040c6610f15da98a1893f66ce467d21dce2
Hamza Baloch <158695894+Hamzabaloch08@users.noreply.github.com>
2025-09-17 19:26:04 +0500

    Update authRoutes.mjs

--- routes/authRoutes.mjs
+++ routes/authRoutes.mjs
@@ -1,11 +1,10 @@
 import express from "express";
-import { signUp, login, logout, check } from "../controllers/authController.mjs";
+import { signUp, login, check } from "../controllers/authController.mjs";
 
 const router = express.Router();
 
 router.post("/signup", signUp);
 router.post("/login", login);
-router.post("/logout", logout);
 router.get("/check", check);
 
 export { router as authRoutes };
