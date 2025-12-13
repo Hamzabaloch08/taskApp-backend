1341ffc4b31a83425c819326149aa786ff92bac8
Hamza Baloch <158695894+Hamzabaloch08@users.noreply.github.com>
2025-12-14 02:22:31 +0500

    Update app.mjs

--- app.mjs
+++ app.mjs
@@ -18,17 +18,17 @@ const allowedOrigins = [
 // Middleware
 app.use(express.json());
 
-// app.use(cors({
-//   origin: function(origin, callback){
-//     if(!origin) return callback(null, true); // allow non-browser requests
-//     if(allowedOrigins.indexOf(origin) === -1){
-//       return callback(new Error("CORS not allowed"), false);
-//     }
-//     return callback(null, true);
-//   },
-//   methods: ["GET","POST","PUT","DELETE","OPTIONS"],
-//   allowedHeaders: ["Content-Type","Authorization"],
-// }));
+app.use(cors({
+  origin: function(origin, callback){
+    if(!origin) return callback(null, true); // allow non-browser requests
+    if(allowedOrigins.indexOf(origin) === -1){
+      return callback(new Error("CORS not allowed"), false);
+    }
+    return callback(null, true);
+  },
+  methods: ["GET","POST","PUT","DELETE","OPTIONS"],
+  allowedHeaders: ["Content-Type","Authorization"],
+}));
 
 app.use((req, res, next) => {
   const publicPaths = [
